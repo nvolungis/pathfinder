f12c4b0568e5257849ad1fee619ce7bbfb45fcdf
Neil Volungis <nvolungis@gmail.com>
2017-11-10 19:54:18 -0500

    draggable, selectable shape

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React  from 'react';
+import Editor from './Editor';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        hi
+        <Editor width={700} height={700} />
       </div>
     );
   }
---
+++ client/src/Editor/index.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import {
+  Stage,
+  Rect,
+  Layer,
+} from 'react-konva';
+
+
+class DownpourStage extends React.Component {
+  render() {
+    const dims = {
+      height: this.props.height,
+      width: this.props.width
+    };
+
+    return (
+      <Stage {...dims}>
+        <Layer>
+          <Rect x={0} y={0} {...dims} onClick={this.props.onClick}/>
+          {this.props.children}
+        </Layer>
+      </Stage>
+    )
+  }
+}
+
+
+class Shape extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isSelected: false,
+      startX: 0,
+      startY: 0,
+      x: 10,
+      y: 10,
+    };
+
+    this.onClick = (e) => {
+      e.cancelBubble = true;
+      this.setState({isSelected: true});
+      this.props.onClick(e);
+    }
+
+    this.onDragEnd = (e) => {
+      this.setState({
+        x: this.state.x + (e.evt.layerX - this.state.startX),
+        y: this.state.y + (e.evt.layerY - this.state.startY),
+      });
+    };
+
+    this.onMouseDown = (e) => {
+      this.setState({
+        startX: e.evt.layerX,
+        startY: e.evt.layerY,
+      });
+    }
+  }
+
+  render() {
+    return (
+       <Rect
+          x={this.state.x}
+          y={this.state.y}
+          width={50}
+          height={50}
+          stroke={this.props.isSelected ? '#0000ff' : '#000000'}
+          strokeWidth={2}
+          onClick={this.onClick}
+          draggable={true}
+          onMouseDown={this.onMouseDown}
+          onDragEnd={this.onDragEnd}
+        />
+    );
+  }
+}
+
+
+
+class Editor extends React.Component {
+  constructor() {
+    super();
+    this.state = {isSelected: false};
+
+    this.onShapeClick = (e) => {
+      this.setState({ isSelected: true });
+    };
+
+    this.onStageClick = (e) => {
+      this.setState({ isSelected: false })
+    };
+  }
+
+
+  render() {
+    const {width, height} = this.props;
+    const dims = {width, height};
+
+    return (
+      <DownpourStage {...dims} onClick={this.onStageClick}>
+        <Shape
+          onClick={this.onShapeClick}
+          isSelected={this.state.isSelected}
+        />
+      </DownpourStage>
+    )
+  }
+}
+
+export default Editor;
